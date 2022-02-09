8d960394303e775d03b761d1dff55361b9c935c7
Pierre Grimaud <grimaud.pierre@gmail.com>
2022-02-09 12:41:47 +0100

    Re-add sentry

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 $config = require_once __DIR__ . '/../config/parameters.php';
 
-//Sentry\init(['dsn' => $config['sentry_key']]);
+Sentry\init(['dsn' => $config['sentry_key']]);
 
 use Bitcoin\{Exchange, Bitcoin, Response};
 use GuzzleHttp\Client as GuzzleClient;
